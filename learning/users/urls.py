794565f7d09291fdf7479321203448ffee17391a
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@192.168.0.6>
2025-09-12 18:02:36 -0300

    add logout function

--- learning/users/urls.py
+++ learning/users/urls.py
@@ -4,4 +4,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     #página de login(auth_views.LoginView.as_view(template_name='users/login.html')serve para renderizar o template de login)
     path('login',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    #página de logout(auth_views.LogoutView.as_view()serve para fazer o logout)
+    path('logout',auth_views.LogoutView.as_view(template_name='users/login.html'),name='logout'),
 ]
