d97a19f9e8000755b1b17411b622b87669731250
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-09-19 12:00:53 -0300

    add outra forma de fazer o logout

--- learning/users/urls.py
+++ learning/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     #página de login(auth_views.LoginView.as_view(template_name='users/login.html')serve para renderizar o template de login)
     path('login',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     #página de logout(auth_views.LogoutView.as_view()serve para fazer o logout)
-    path('logout',auth_views.LogoutView.as_view(template_name='users/login.html'),name='logout'),
+    #path('logout',auth_views.LogoutView.as_view(template_name='users/login.html'),name='logout'),
+    path('logout',views.logout_view,name='logout'),
 ]
--- learning/users/views.py
+++ learning/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
 # Create your views here.
+def logout_view(request):
+    logout(request) # Função para fazer logout do usuário
+    return HttpResponseRedirect(reverse('index')) # Redireciona para a página inicial após logout
